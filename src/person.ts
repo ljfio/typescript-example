18980337f6cdda6519d9a0dc1258446e6fd50086
Luke Fisher <mail@lukefisher.me>
2021-02-02 22:22:02 +0000

    Add in documentation

--- src/person.ts
+++ src/person.ts
@@ -1,10 +1,18 @@
 export class Person {
     private name: string;
 
+    /**
+     * Construct an instance of a person
+     * @param name Name of the person
+     */
     constructor(name: string) {
         this.name = name;
     }
 
+    /**
+     * Produce greeting message
+     * @returns greeting for the person
+     */
     greet() {
         return `Hello, ${this.name}!`;
     };
